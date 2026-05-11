4d056d08d71e5fc28305808e90fa4dd9b177b76c
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2026-05-11 13:00:47 +0700

    fix: static files and gunicorn

--- aeromiles/settings.py
+++ aeromiles/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
